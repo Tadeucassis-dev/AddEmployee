193b09d11e632746c26f3b0e217f10b9a5d870c7
tadeu.cesar <tadeu.cesar@worldconnectoficial.com.br>
2023-08-28 23:39:09 -0300

    update sidebar and icon bar

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { ChakraProvider } from '@chakra-ui/react';
+import theme from '@chakra-ui/theme';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Register from './Register';
+import AddEmployee from './pages/AddEmployee'; 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ReactDOM.render(
+  <Router>
+    <ChakraProvider theme={theme}>
+      <React.StrictMode>
+        <Routes> 
+          <Route path="/" element={<Register />} /> 
+          <Route path="/Add-employee" element={<AddEmployee />} />
+        </Routes>
+      </React.StrictMode>
+    </ChakraProvider>
+  </Router>,
+  document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
